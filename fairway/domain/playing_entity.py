af8ec2fcbdf517094061df9acea79029b0b33089
giacobenin <giacomo.benincasa@gmail.com>
2019-03-18 22:18:44 -0400

    Added constructor parameters

--- fairway/domain/playing_entity.py
+++ fairway/domain/playing_entity.py
@@ -3,9 +3,9 @@ from abc import ABC, abstractmethod
 
 class PlayingEntity(ABC):
 
-    def __init__(self):
-        self._expected_score = 0.0
-        self._prob_of_winning = 0.0
+    def __init__(self, expected_score=0.0, prob_of_winning=0.0):
+        self._expected_score = expected_score
+        self._prob_of_winning = prob_of_winning
 
     @property
     def expected_score(self):
