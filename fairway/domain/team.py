ac0dd1669f776c7bc6670c557b317f5f5d30520b
giacobenin <giacomo.benincasa@gmail.com>
2019-03-18 22:19:46 -0400

    Improved string representation. Added utility functions

--- fairway/domain/team.py
+++ fairway/domain/team.py
@@ -1,4 +1,5 @@
 import uuid
+from collections import defaultdict
 from typing import Iterable
 
 from fairway.domain.player import Player
@@ -16,10 +17,10 @@ class Team(PlayingEntity):
         self._team_id = team_id
 
     def __repr__(self):
+        sorted_handicaps = sorted([int(player.handicap) for player in self._member_players])
         return "{} {} - Win Prob: {}. Expected score: {}. Handicaps: {}"\
             .format(self.__class__.__name__, self._team_id,
-                    self.prob_of_winning, self.expected_score,
-                    ','.join([str(player.handicap) for player in self._member_players]))
+                    self.prob_of_winning, self.expected_score, ','.join([str(h) for h in sorted_handicaps]))
 
     @classmethod
     def create(cls):
@@ -54,3 +55,24 @@ class Team(PlayingEntity):
     def remove_player(self, player: Player):
         assert isinstance(player, Player)
         self._member_players = [member for member in self._member_players if member.id != player.id]
+
+
+def create_teams_from_pre_assigned_players(players: Iterable[Player]) -> Iterable[Team]:
+    # Group players by team
+    players_by_team = defaultdict(list)
+    for player in players:
+        assert isinstance(player, Player)
+        if player.team_id:
+            players_by_team[player.team_id].append(player)
+        else:
+            assert False, "Player {} does not belong to any team".format(player.id)
+
+    # Create teams and set members
+    teams = tuple(Team.create() for i in range(len(players_by_team)))
+    for team, team_members in zip(teams, players_by_team.values()):
+        team.add_players(team_members)
+
+
+def create_empty_teams(number_of_teams: int) -> Iterable[Team]:
+    assert isinstance(number_of_teams, int)
+    return tuple(Team.create() for i in range(number_of_teams))
