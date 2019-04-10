0cf87a5f9536cb28d9ea1b7fe3d2958f33aa691a
giacobenin <giacomo.benincasa@gmail.com>
2019-04-10 01:16:02 -0400

    Fixed bugs. Removed obsolete code

--- fairway/usecases/bestball.py
+++ fairway/usecases/bestball.py
@@ -25,11 +25,11 @@ class BestBallGame(Game):
         self._number_of_best_balls = number_of_best_balls
 
     @property
-    def number_of_best_balls(self):
+    def number_of_best_balls(self) -> int:
         return self._number_of_best_balls
 
     @property
-    def number_of_holes(self):
+    def number_of_holes(self) -> int:
         return self._number_of_holes
 
     def play_individual_game(self, players: Iterable[Player]):
@@ -37,6 +37,130 @@ class BestBallGame(Game):
 
     def play_team_game(self, players: Iterable[Player], teams: Iterable[Team]):
         self._play(count_game_wins(), players, teams)
+
+    def _play2(self, counter_fn, players, teams=None) -> Iterable[PlayingEntity]:
+        def _scenario(allowances: np.ndarray, player_handicaps: Iterable[int], teams=None, aggregate_by=None):
+            while True:
+                sample = self.simulator.sample_game_scenario(player_handicaps, self.number_of_holes)
+                s = np.add(sample, allowances)
+                if teams:
+                    s = self.to_team_scenario(s, teams)
+                yield s
+
+        # Vectorize objects for faster processing:
+
+        # 1. Sort players by teams (simplifies slicing by group)
+        players_by_team = sorted(players, key=lambda p: p.team_id)
+        index_to_player_id = dict()
+        player_id_to_index = dict()
+        for index, player in enumerate(players_by_team):
+            index_to_player_id[index] = player.id
+            player_id_to_index[player.id] = index
+        player_handicaps = tuple(player.handicap for player in players_by_team)
+        all_allowances = np.vstack((player.allowances_by_hole for player in players_by_team))  # Pre-compute allowance matrix
+        assert (all_allowances.shape == (len(players_by_team), self._number_of_holes))
+
+        # Create list of tuples, each of them containing the indexes of its members
+        teams_as_player_indexes = list()
+        for team in teams:
+            teams_as_player_indexes.append(tuple(player_id_to_index[player.id] for player in team.members))
+            teams_as_player_indexes = [sorted(t) for t in teams_as_player_indexes]
+
+        # Algorithm
+        individual = True
+        self.simulator.reset()
+        tot_scores = None
+        number_of_wins = dict()
+        next_scenario = _scenario(all_allowances, player_handicaps, teams_as_player_indexes, counter_fn)
+        player_scores_by_hole = np.zeros(type=float)
+        number_of_wins_by_hole = np.zeros(type=float)
+        for count, scenario in enumerate(next_scenario):
+            if count >= self.simulator.number_of_iterations:
+                break
+            player_scores_by_hole = np.add(player_scores_by_hole, scenario)
+            for i in range(18):
+                hole_scores = scenario[:, i]
+                if individual:
+                    winning_score = min(hole_scores)
+                    winning_players = tuple(idx for idx in range(len(hole_scores)) if hole_scores[idx] == winning_score)
+                    for winning_player in winning_players:
+                        number_of_wins_by_hole[winning_player, i] += 1.0 / float(len(winning_players))
+
+    def _play_individual_or_team_games(self, allowances, player_handicaps, teams=None):
+        """
+        Runs self.simulator.number_of_iterations games and returns average scores by hole, prob. of winning by hole,
+        average game score, prob. of winning game.
+
+        :param allowances:
+        :param player_handicaps:
+        :param teams:
+        :return:
+        """
+        if teams:
+            fn = self._play_team_game
+            scores_by_hole = np.zeros(len(teams), self.number_of_holes, type=float)
+            wins_by_hole = np.zeros(len(teams), self.number_of_holes, type=float)
+        else:
+            fn = self._play_individual_game
+            scores_by_hole = np.zeros(len(player_handicaps), self.number_of_holes, type=float)
+            wins_by_hole = np.zeros(len(player_handicaps), self.number_of_holes, type=float)
+
+        for count, sample in enumerate(self.simulator.sample_game_scenario(player_handicaps, self.number_of_holes)):
+            if count >= self.simulator.number_of_iterations:
+                break
+            scenario = np.add(sample, allowances)
+
+            scores_by_hole, wins_by_hole = fn(scenario, scores_by_hole, wins_by_hole)
+
+        average_game_score = np.divide(np.sum(scores_by_hole, axis=1), self.simulator.number_of_iterations)
+        scores_by_hole = np.divide(scores_by_hole, self.simulator.number_of_iterations)
+        prob_of_winning_by_hole = np.divide(wins_by_hole, self.simulator.number_of_iterations)
+        return scores_by_hole, average_game_score, prob_of_winning_by_hole
+
+    def _play_individual_game(self, scenario, player_scores_by_hole, number_of_wins_by_hole):
+        """
+        It updates player_scores_by_hole, and number_of_wins_by_hole (to avoid creating a new array)!!!
+
+        :param scenario: the current scenario (scores of all players on all holes for one iteration)
+        :param player_scores_by_hole:
+        :param number_of_wins_by_hole:
+        :return:
+        """
+        player_scores_by_hole = np.add(player_scores_by_hole, scenario)
+        return player_scores_by_hole, self.count_wins_by_hole(scenario, number_of_wins_by_hole)
+
+    def _play_team_game(self, scenario, teams, team_scores_by_hole, number_of_wins_by_hole):
+        """
+        It updates player_scores_by_hole, and number_of_wins_by_hole (to avoid creating a new array)!!!
+
+        :param scenario:
+        :param teams:
+        :param team_scores_by_hole:
+        :param number_of_wins_by_hole:
+        :return:
+        """
+        def get_team_score_on_hole(current_team_scenario, current_hole):
+            all_members_scores = current_team_scenario[:, current_hole]
+            idx = np.argpartition(current_team_scenario[:, current_hole], self.number_of_best_balls)
+            return sum(all_members_scores[idx[:self.number_of_best_balls]])
+
+        team_scenario = np.zeros(len(teams), self.number_of_holes, type=float)
+        for i, team in enumerate(teams):
+            all_members_scenario = scenario[team[0]:team[-1], :]  # This expects team members' scores in contiguous rows
+            team_scenario[i] = [get_team_score_on_hole(all_members_scenario, hole) for hole in range(self.number_of_holes)]
+
+        self.count_wins_by_hole(team_scenario, number_of_wins_by_hole)
+        return np.add(team_scores_by_hole, team_scenario), number_of_wins_by_hole
+
+    def count_wins_by_hole(self, scenario, number_of_wins_by_hole):
+        for i in range(self.number_of_holes):
+            hole_scores = scenario[:, i]
+            winning_score = min(hole_scores)
+            winning_entities = tuple(idx for idx in range(len(hole_scores)) if hole_scores[idx] == winning_score)
+            for winning_entity in winning_entities:
+                number_of_wins_by_hole[winning_entity, i] += 1.0 / float(len(winning_entities))
+        return number_of_wins_by_hole
+
 
     def _play(self, counter_fn, players, teams=None) -> Iterable[PlayingEntity]:
         # Vectorize objects for faster processing:
@@ -59,10 +183,8 @@ class BestBallGame(Game):
         self.simulator.reset()
         tot_scores = None
         number_of_wins = dict()
-
-        for tot_scores, number_of_wins in limit(self.simulator.number_of_iterations, self._scenario(all_allowances,
-                                                                                                    player_handicaps, teams_as_player_indexes,
-                                                                                                    counter_fn)):
+        scenario_gen = self._scenario(all_allowances, player_handicaps, teams_as_player_indexes, counter_fn)
+        for tot_scores, number_of_wins in limit(self.simulator.number_of_iterations, scenario_gen):
             pass
         counter_fn.close()
 
@@ -80,92 +202,11 @@ class BestBallGame(Game):
 
         return teams if teams else players
 
-    def _scenario(self, allowances: np.ndarray, player_handicaps: Iterable[int], teams=None, aggregate_by=None):
-        while True:
-            sample = self.simulator.sample_game_scenario(player_handicaps, self.number_of_holes)
-            s = np.add(sample, allowances)
-            if teams:
-                s = self.to_team_scenario(s, teams)
-
-            if aggregate_by:
-                yield aggregate_by.send(s)
-            else:
-                yield s
-
-    def to_team_scenario(self, players_scenarios, teams):
-        n_players, n_holes = players_scenarios.shape
-        team_scenarios = np.zeros((len(teams), n_holes), dtype=int)
-        players_scenarios_tr = players_scenarios.transpose()
-        for team_index, player_indexes in enumerate(teams):
-            for hole in range(n_holes):
-                all_team_scores = np.asarray(itemgetter(*player_indexes)(players_scenarios_tr[hole]), dtype=int)
-                if self._number_of_best_balls < len(player_indexes):
-                    indexes = np.argpartition(all_team_scores, self.number_of_best_balls)
-                    team_best_scores_sum = np.sum(all_team_scores[indexes[:self.number_of_best_balls]])
-                else:
-                    team_best_scores_sum = np.sum(all_team_scores)
-                team_scenarios[team_index, hole] = team_best_scores_sum
-
-        return team_scenarios
 
 
-@coroutine
-def count_hole_wins():
-    """
-
-    :return: the number of holes that a player won, along with their scores
-    """
-    tot_scores = None
-    curr_counts = dict()
-    while True:
-        game_scenario = (yield (tot_scores, curr_counts))
-        n_players, n_holes = game_scenario.shape
-
-        # Update Scores
-        if tot_scores is None:
-            tot_scores = np.zeros(n_players, dtype=int)
-        _, tot_scores = _update_scores(game_scenario, tot_scores)
-
-        # Update Hole Win Counts
-        for hole in range(n_holes):
-            winners = _find_winners(game_scenario[:, hole])
-            for winner in winners:
-                curr_counts[winner] = curr_counts.get(winner, 0) + (1.0 / len(winners))
 
 
-@coroutine
-def count_game_wins():
-    """
-
-    :return: the number of games that a team won, along with their scores
-    """
-    tot_scores = None
-    curr_counts = dict()
-    while True:
-        game_scenario = (yield (tot_scores, curr_counts))
-
-        # Update Scores
-        if tot_scores is None:
-            n_players, _ = game_scenario.shape
-            tot_scores = np.zeros(n_players, dtype=int)
-        scores, tot_scores = _update_scores(game_scenario, tot_scores)
-
-        # Update Game Win Counts
-        winners = _find_winners(scores)
-        for winner in winners:
-            curr_counts[winner] = curr_counts.get(winner, 0) + (1.0/len(winners))
 
 
-def _update_scores(game_scenario, current_tot_scores):
-    n_players, _ = game_scenario.shape
-    scores = np.zeros(n_players)
-    for player_index in range(n_players):
-        scores[player_index] = np.sum(game_scenario[player_index, :])
-
-    tot_scores = np.add(current_tot_scores, scores)
-    return scores, tot_scores
 
 
-def _find_winners(scores):
-    winning_score = np.min(scores)
-    return tuple(idx for idx in range(len(scores)) if scores[idx] == winning_score)
